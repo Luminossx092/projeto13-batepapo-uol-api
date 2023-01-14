6b31c44413f3352210821f7d59b5e48f1759235c
luminossx092 <hehayase@gmail.com>
2023-01-14 13:16:40 -0300

    versao corrigida 1

--- src/app.js
+++ src/app.js
@@ -72,9 +72,11 @@ server.post("/messages", async (req, res) => {
 
 server.get("/messages", async (req, res) => {
     const limit = parseInt(req.query.limit)
+    if(limit<=0)return res.sendStatus(422)
     try {
         const response = await db.collection("messages")
             .find({ $or: [{ from: req.headers.user }, { type: { $not: /private_message/ } }] })
+            .sort({_id:-1})
             .limit(limit)
             .toArray()
         return res.send(response)
